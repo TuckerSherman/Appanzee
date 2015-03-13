7e81a1a2d67a8f9f605c60ef2629829f8a521d6b
Sherman, Tucker (ETW) <Tucker.Sherman@nike.com>
2015-03-13 13:16:09 -0700

    demo feature steps added, skeleton for screen size on class added

--- features/devices/simulator.rb
+++ features/devices/simulator.rb
@@ -1,42 +1,63 @@
+#  Created by Tucker Sherman on 3/13/2015.
+#  Copyright (c) 2015 Apache licence 2.0
 
 class Simulator
- 
-def foreground
-   `osascript -e 'tell application "System Events" to tell process "iOS Simulator"' -e 'set frontmost to true' -e 'end tell'`
-end
-  
-def start_highway_drive
-  foreground
-  exit = `osascript -e 'tell application "System Events" to tell process "iOS Simulator"' -e'click menu bar item "Debug" of menu bar 1' -e'delay 0.1' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 124' -e'key code 125'-e'key code 125'-e'key code 125'-e'key code 125'-e'key code 125' -e'keystroke "" & return' -e'delay 2' -e'end tell'`
-  puts("highway drive start - #{exit.to_i}")
-end
-
-def stop_highway_drive
-  foreground
-   exit = `osascript -e 'tell application "System Events" to tell process "iOS Simulator"' -e'click menu bar item "Debug" of menu bar 1' -e'delay 0.1' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 124' -e'keystroke "" & return' -e'delay 2' -e'end tell'`
-  puts("highway drive stop exits - #{exit.to_i}") 
-end
-
-def hit_home
-  foreground
-  exit =`osascript -e 'tell application "System Events" to tell process "iOS Simulator"' -e'keystroke "H" using command down' -e'delay 2' -e'end tell'`
-  puts("home button exits - #{exit.to_i}")
-end
-
-def doubletap_home
-  foreground
-  exit = `osascript -e 'tell application "System Events" to tell process "iOS Simulator"' -e'keystroke "HH" using command down' -e'delay 2' -e'end tell'`
-  puts("home button exits - #{exit.to_i}")
-end
-
-#IPHONE 6 SPECIFIC TODO: make these dynamic
-def springboard_page_switch_to(number)
-  case number
-    when 1
-      $driver.execute_script 'mobile: tap', :x =>181, :y => 564
-    when 2
-      $driver.execute_script 'mobile: tap', :x =>197, :y => 564
+  def initialize (device)
+    @device = device
+    case device
+    when "iPhone 4"
+      #TODO: set these screen sizes
+      @screenSize = ()
+    when "iPhone 4"
+      @screenSize = ()
+    when "iPhone 5"
+      @screenSize = ()
+    when "iPhone 5s"
+      @screenSize = ()
+    when "iPhone 6"
+      @screenSize = ()
+    when "iPhone 6+"
+      @screenSize = ()
+    end
+    
   end
+  
+  def foreground
+     `osascript -e 'tell application "System Events" to tell process "iOS Simulator"' -e 'set frontmost to true' -e 'end tell'`
+  end
+
+  def start_highway_drive
+    foreground
+    exit = `osascript -e 'tell application "System Events" to tell process "iOS Simulator"' -e'click menu bar item "Debug" of menu bar 1' -e'delay 0.1' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 124' -e'key code 125'-e'key code 125'-e'key code 125'-e'key code 125'-e'key code 125' -e'keystroke "" & return' -e'delay 2' -e'end tell'`
+    puts("highway drive start - #{exit.to_i}")
+  end
+
+  def stop_highway_drive
+    foreground
+     exit = `osascript -e 'tell application "System Events" to tell process "iOS Simulator"' -e'click menu bar item "Debug" of menu bar 1' -e'delay 0.1' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 125' -e'key code 124' -e'keystroke "" & return' -e'delay 2' -e'end tell'`
+    puts("highway drive stop exits - #{exit.to_i}") 
+  end
+
+  def hit_home
+    foreground
+    exit =`osascript -e 'tell application "System Events" to tell process "iOS Simulator"' -e'keystroke "H" using command down' -e'delay 2' -e'end tell'`
+    puts("home button exits - #{exit.to_i}")
+  end
+
+  def doubletap_home
+    foreground
+    exit = `osascript -e 'tell application "System Events" to tell process "iOS Simulator"' -e'keystroke "HH" using command down' -e'delay 2' -e'end tell'`
+    puts("home button exits - #{exit.to_i}")
+  end
+
+  #IPHONE 6 SPECIFIC TODO: make these dynamic
+  def springboard_page_switch_to(number)
+    case number
+      when 1
+        $driver.execute_script 'mobile: tap', :x =>181, :y => 564
+      when 2
+        $driver.execute_script 'mobile: tap', :x =>197, :y => 564
+    end
   
 end
 
---
+++ features/step_definitions/simulator_steps.rb
@@ -0,0 +1,54 @@
+#  Created by Tucker Sherman on 3/13/2015.
+#  Copyright (c) 2015 Apache licence 2.0
+
+Given(/^Move to Apple$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^Start bicycle ride$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^Start city run$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^Start highway drive$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^Stop simulating location$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^Shake the phone$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^Trigger low memory warning$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^hit the home button$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^switch to springboard page (\d+)$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^re\-open the app$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^rotate right$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^rotate left$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^color blended layers$/) do
+  pending # express the regexp above with the code you wish you had
+end
